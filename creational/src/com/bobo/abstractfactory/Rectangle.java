96988a968e2e5b9f55dd028aa5327ba92691738a
bobo.dou <bobo.dou@weimob.com>
2018-07-17 16:07:53 +0800

    抽象工厂模式

---
+++ creational/src/com/bobo/abstractfactory/Rectangle.java
@@ -0,0 +1,13 @@
+package com.bobo.abstractfactory;
+
+/**
+ * Created by bobo.dou@weimob.com on 2018/7/17 15:45
+ *
+ * 实现图形的接口
+ */
+public class Rectangle implements Shape{
+    @Override
+    public void draw() {
+        System.out.println("draw Rectangle ");
+    }
+}
---
+++ creational/src/com/bobo/abstractfactory/Shape.java
@@ -0,0 +1,11 @@
+package com.bobo.abstractfactory;
+
+/**
+ * Created by bobo.dou@weimob.com on 2018/7/17 15:44
+ *
+ * 图形接口
+ */
+public interface Shape {
+
+    void draw();
+}
